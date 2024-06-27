246d4faaed0ab18a10d68ce67d7a7f7f72868caa
BOOMIKA R <BOOMIKA R@Boomika>
2024-06-27 12:00:29 +0530

    2nd project

---
+++ JavaBasic/src/DAY3/CharDemo.java
@@ -0,0 +1,24 @@
+package DAY3;
+
+public class CharDemo {
+	public static void main(String[]args) {
+		char ch = 'a';
+		System.out.println(ch);
+		
+		char ch1 = 65;
+		System.out.println(ch1);
+		
+		char varl = '\u00A7';
+		System.out.println(varl);
+		
+		char var2 = '\u20AC';
+		System.out.println(var2);
+		
+		int a = 'A';
+		System.out.println(a);
+		
+		
+	
+	}
+
+}
