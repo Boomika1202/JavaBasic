4646e5bb6c5c19e48b495adc96739ab6a4f15133
BOOMIKA R <BOOMIKA R@Boomika>
2024-06-27 11:02:59 +0530

    This is my Core Java Project

---
+++ JavaBasic/src/DAY1/AdditionProgram.java
@@ -0,0 +1,12 @@
+package DAY1;
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JFS");
+		System.out.println("MVIT");
+		System.out.print("TNSIF");
+
+	}
+
+}
